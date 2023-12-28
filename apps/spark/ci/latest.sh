a8b11947e6f598fffc687537c9bbe2e7c54df2d8
michael <michael@feldblyum.com>
2023-12-27 19:02:25 -0500

    spark

--- apps/spark/ci/latest.sh
+++ apps/spark/ci/latest.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+#this work?
 version=$(curl -s "https://registry.hub.docker.com/v2/repositories/library/spark/tags?ordering=name" | jq --raw-output --arg s "3.5.0" '.results[] | select(.name | contains($s)) | .name' 2>/dev/null | head -n1)
 printf "%s" "${version}"
